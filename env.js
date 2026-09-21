fcac2c5977d7a344d1c03c066ee81e097537518d
cafeSowoo <harminis@gmail.com>
2026-09-21 16:29:46 +0900

    Configure tennisbom.com custom domain

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 window.TENNIS_CONFIG = window.TENNIS_CONFIG || {
   supabaseUrl: "https://myincubzgvhyreyqbban.supabase.co",
   supabaseAnonKey: "sb_publishable_6NmnzRERi_Azsyh44zbZnw_HjqHh7hd",
-  siteUrl: "https://cafesowoo.github.io/tennis-homepage-v2/",
+  siteUrl: "https://tennisbom.com/",
   ownerEmail: "harminis@gmail.com",
   allowRemoteWrites: true,
   reviewMode: true
